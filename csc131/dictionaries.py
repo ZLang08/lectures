8ba59a07b319b4c16bf7f3f062767bdabc5dd904
ZLang08 <zl08@live.missouristate.edu>
2018-01-26 13:11:25 -0600

    Lecture of Friday, Jan. 26

---
+++ csc131/dictionaries.py
@@ -0,0 +1,6 @@
+def get_personal_data() -> dict:
+    """
+    :return: Returns a dictionary of personal information.
+    """
+    personal_data = {"name": "Zach", "role": "student"}
+    return personal_data
