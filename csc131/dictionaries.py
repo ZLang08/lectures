beef7bae976eae295bbdf132eee18e6ff0d6d5a6
ZLang08 <zl08@live.missouristate.edu>
2018-01-29 13:13:20 -0600

    Lecture of Friday, Jan. 26

--- csc131/dictionaries.py
+++ csc131/dictionaries.py
@@ -1,6 +1,31 @@
+import string
+
+
 def get_personal_data() -> dict:
     """
     :return: Returns a dictionary of personal information.
     """
-    personal_data = {"name": "Zach", "role": "student"}
+    personal_data = {'name': 'Zach', 'role': 'student'}
     return personal_data
+
+
+def main() -> int:
+    default_dict = dict()
+    print(default_dict)
+    initialized_dict = dict([('name', 'Zach'), ('a_role', 'prankster')])
+    print(initialized_dict)
+    simple_init_dict = dict(name='Zach', a_role='student')
+    print(simple_init_dict)
+    simple_init_dict['a_role'] = 'prankster'
+    print(simple_init_dict)
+    my_comprehension = {x: x**2 for x in range(5)}
+    print(my_comprehension)
+
+    s = "a little lamb, it's fleece".translate({ord(i): None for i in string.punctuation})
+    print(s)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
